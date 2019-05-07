bef9e00998850332281337648e4f9fd209a25f9d
ekiprop <evanskiprop1@gmail.com>
2019-05-07 16:17:34 +0300

    Relationship creation btn product and review

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,12 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'detail', 'price', 'stock', 'discount'];
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }

26414fd1596a4bd73361f0c4f98d5da764650d87
ekiprop <evanskiprop1@gmail.com>
2019-05-09 09:33:59 +0300

    Review update and delete

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -15,6 +15,7 @@ class ReviewResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'cutomer' => $this->customer,
             'body' => $this->review,
             'star' => $this->star
